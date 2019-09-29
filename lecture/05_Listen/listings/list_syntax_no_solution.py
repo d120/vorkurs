130711310289c46aaf0104a94d27b9e17a72a98c
Jonas Kapitzke <mail@jkapitzke.de>
2019-09-29 18:20:17 +0200

    Lösungen bei manchen Beispielen noch nicht zeigen, damit Dozent*in es vor dem Plenum selber laut durchdenken kann.

---
+++ lecture/05_Listen/listings/list_syntax_no_solution.py
@@ -0,0 +1,13 @@
+# Liste erstellen
+meine_liste = [42,56,1,2,5]
+leere_liste = []
+
+# Liste lesen
+# liest Element an Index 1 aus
+zweites_element = meine_liste[1]  
+letztes_element = meine_liste[-1] 
+
+# Element setzen
+# setzt Element an Index 3 auf den Wert 9
+meine_liste[3] = 9;
+# meine_liste ist jetzt: 
