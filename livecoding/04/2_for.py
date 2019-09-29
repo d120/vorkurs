b3edadbd178767bbc45a53b203e0d1d7619d5075
Steffen Klee <steffen.klee@fachschaft.informatik.tu-darmstadt.de>
2019-09-29 21:59:19 +0200

    livecoding/04: Removed some old Java code

--- livecoding/04/2_for.py
+++ livecoding/04/2_for.py
@@ -1,31 +1,3 @@
-public class mehrSchleifen{
-  public static void main(String[] args){
-    for(int zaeler = 0; zaeler < 100; zaeler++){
-      System.out.print(zaeler + " ");
-    }
-
-    //nach dem ersten ausfüren:
-    for(int ausen = 0; ausen < 100; ausen++){
-      System.out.println();
-      for(int zaeler = 0; zaeler < 100; zaeler++){
-        System.out.print(zaeler + " ");
-      }
-      //int notOK = 1;
-    }
-    //System.out.println(notOK);
-
-    System.out.println();
-    System.out.println();
-
-    String input = "";
-    do{
-      System.out.println("ende?");
-      input = Input.readString();
-    }while(!input.equals("ende"));
-  }
-}
- 
-
 for i in range(100):
     print(i)
     
