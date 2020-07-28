da9ee1c47cd94bcb2a5635fd9f7ff2f641ecbcef
kotto <Kevin@KevinOtto.de>
2020-07-28 12:36:33 +0200

    [new TU-Design] fix formating

--- lecture/05_Listen/listings/partlySlice.py
+++ lecture/05_Listen/listings/partlySlice.py
@@ -3,6 +3,5 @@ liste = ['Banane', 'Apfel', 'Ei', 'Käse', 'Zitrone', 'Ingwer']
 sliced = liste[:4]
 #sliced = ['Banane', 'Apfel', 'Ei', 'Käse']
 
-
 sliced2 = liste[2:]
 #sliced2 = ['Ei', 'Käse', 'Zitrone', 'Ingwer']
--- lecture/05_Listen/listings/partlySlice_no_solution.py
+++ lecture/05_Listen/listings/partlySlice_no_solution.py
@@ -1,8 +1,6 @@
 liste = ['Banane', 'Apfel', 'Ei', 'Käse', 'Zitrone', 'Ingwer']
 
 sliced = liste[:4]
-	
 
 
 sliced2 = liste[2:]
-	
