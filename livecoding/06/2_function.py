c0e3b45bdae303892190dc4c771bcc01b08deaa8
jnnkB <29376715+jnnkB@users.noreply.github.com>
2020-10-22 15:38:42 +0200

    fixed typo in 2_function.py

--- livecoding/06/2_function.py
+++ livecoding/06/2_function.py
@@ -7,10 +7,8 @@ def lies_zahl():
 n_zahlen = int(input("Wie viele Zahlen sollen gelesen werden? "))
 zahlen = []  # Speichert alle Zahlen
 
-if n_zahlen > 0::
-    for i in range(n_zahlen):
-        zahlen.append(lies_zahl(n_elemente))
-        
+if n_zahlen > 0:
+    for _ in range(n_zahlen):
     print("Fertig!")
     
     # Berechne summe
