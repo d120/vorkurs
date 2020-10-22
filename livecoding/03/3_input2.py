eee36358b63c4db44fd9f405c3fbb4a28c7ef4b2
jnnkB <29376715+jnnkB@users.noreply.github.com>
2020-10-22 15:39:13 +0200

    fixed typos in 3_input2.py and 2_input.py

--- livecoding/03/3_input2.py
+++ livecoding/03/3_input2.py
@@ -2,14 +2,15 @@ name = input("Bitte gib deinen Namen ein: ")
 alter = input("Bitte gib dein Gewicht ein: ")
 groesse = input("Bitte gib deine Größe ein: ")
 
-print(name + " ist " + alter " Jahre alt und " + groesse + "m groß.")
+print(name + " ist " + alter + " Jahre alt und " + groesse + "m groß.")
 
 # Dies ist neu
 alter = int(alter)
 alter += 1
-print(name + " ist nächstes Jahr " + str(alter) " Jahre alt")
+print(name + " ist nächstes Jahr " + str(alter) + " Jahre alt")
 # Erklärung: alter ist noch ein String, also erst umwandeln in Zahl
-# Zuerst ohne Konvertierung zeigen und erklären was passiert. Ebenfalls die Konvertierung von alter weglassen.
+# Zuerst ohne Konvertierung zeigen und erklären was passiert.
+# Ebenfalls die Konvertierung von alter weglassen.
 
 groesse = float(groesse)
 # Erklärung: groesse ist noch ein String, also erst umwandeln in Zahl
