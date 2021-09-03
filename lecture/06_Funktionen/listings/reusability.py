25ce9c14e2596a38c0f97ca463f5d1eb529b1a12
Jonas Honermann <jonas.honermann@gmx.de>
2021-09-03 12:05:51 +0200

    fixed slides formatting, partially undid PEP8 to maintain readability of slide examples

--- lecture/06_Funktionen/listings/reusability.py
+++ lecture/06_Funktionen/listings/reusability.py
@@ -1,15 +1,9 @@
 # Volumen eines Zylinders berechnen
 def zylinder_vol(r, h, pi):
     return kreis_flaeche(r, pi) * h
-
 # Oberfläche einer Kugel berechen
-
-
 def kugel_oberfl(r, pi):
     return 4 * kreis_flaeche(r, pi)
-
 # Oberfläche einer Halbkugel berechnen
-
-
 def hemisp_oberfl(r, pi):
     return kugel_oberfl(r, pi) / 2 + kreis_flaeche(r, pi)
