b9c619ce3cff9e7a226ccfe54faf283486d6ca40
Alexander Städing <alexanderstaeding@gmail.com>
2023-09-30 23:58:13 +0200

    Work on lecture 5

---
+++ lecture/05_Listen/listings/list_syntax_immutable_no_type.kts
@@ -0,0 +1,8 @@
+// Liste erstellen
+val myList = listOf$\tikzmark{notype}$(42, 56, 1, 2, 5)
+
+// Leere Liste erstellen
+val leereListe1 = listOf<Int>()
+
+// Geht auch so
+val leereListe2 = emptyList<Int>()
---
+++ lecture/05_Listen/listings/no_list_for.kts
@@ -0,0 +1,4 @@
+val quadrat = mutableListOf<Int>()
+for (i in 0 until 10) {
+    quadrat.add(i * i)
+}
