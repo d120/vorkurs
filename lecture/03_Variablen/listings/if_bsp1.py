f1bdabe6eaa6afb2f2cd98803cf4bca77141f358
Tristan Schulz <tristanschulz1200@gmail.com>
2024-09-22 19:36:06 +0200

    Variablen überarbeitet

---
+++ lecture/03_Variablen/listings/if_bsp1.py
@@ -0,0 +1,11 @@
+alter: int = 17
+
+# Darf ich in Deutschland alleine Auto fahren?
+if alter >= 18:
+    print("Ja! Aber nur mit Fuehrerschein.")
+
+alter = alter + 1
+
+# Darf ich jetzt in Deutschland alleine Auto fahren?
+if alter >= 18:
+    print("Ja! Aber immer noch nur mit Fuehrerschein.")
---
+++ lecture/03_Variablen/listings/if_else_if_example.py
@@ -0,0 +1,6 @@
+if a == b:
+    print("a ist gleich b")
+elif a > b:
+    print("a ist größer als b")
+else:
+    print("b ist größer als a")
