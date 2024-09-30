bc09cde22f8224843a0c733054cfa746fbe062f1
KTuff <git.magha@protonmail.com>
2024-09-30 17:42:45 +0200

    Pest & Kotlin ausgelöscht

---
+++ exercises/code/3_1_d_2_sol.py
@@ -0,0 +1,15 @@
+prime : int = int(input("Gebe eine Zahl ein: "))
+
+isPrime : bool = True
+if prime <= 1:
+    isPrime = False
+else:
+    for i in range(2, prime):
+        if prime % i == 0:
+            isPrime = False
+            break
+
+if isPrime:
+    print(f"{prime} ist eine Primzahl.")
+else:
+    print(f"{prime} ist keine Primzahl.")
